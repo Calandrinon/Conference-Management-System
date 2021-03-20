379678976abe23f8f3c69779b3b4f833251a72a3
danie <danielstanpop@gmail.com>
2021-03-20 17:42:17 +0200

    remove error

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,6 +3,5 @@ package com.example.demo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        HelloServlet helloServlet = new HelloServlet();
     }
 }
