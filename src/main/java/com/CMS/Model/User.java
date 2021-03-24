7918c5d69d874f33987cc291245918a501d24c8b
Calandrinon <georgesut@yahoo.com>
2021-03-24 14:52:45 +0200

    Add DB details to persistence.xml and sample code in example.demo.Main
    Hibernate fails and complains about the User class being non-existent.
    Error: Unknown entity: com.CMS.Model.User

--- src/main/java/com/CMS/Model/User.java
+++ src/main/java/com/CMS/Model/User.java
@@ -2,22 +2,17 @@ package com.CMS.Model;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 
 @Entity
-@Table(name="User")
+@Table(name="_User_")
 public class User {
     @Id
+    @Column(name = "id")
     @GeneratedValue(generator = "increment")
     @GenericGenerator(name = "increment", strategy = "increment")
     private Long id;
-    private String fullName;
-    private String salt;
-    private String password;
-    private String userType;
 
     public void setId(Long id) {
         this.id = id;
@@ -27,6 +22,9 @@ public class User {
         return id;
     }
 
+    @Column(name = "fullName")
+    private String fullName;
+
     public String getFullName() {
         return fullName;
     }
@@ -35,11 +33,35 @@ public class User {
         this.fullName = fullName;
     }
 
+    @Column(name = "userType")
+    private String userType;
     public String getUserType() {
         return userType;
     }
 
     public void setUserType(String userType) {
         this.userType = userType;
+    }
+
+    @Column(name = "salt")
+    private String salt;
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Column(name = "Password")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
