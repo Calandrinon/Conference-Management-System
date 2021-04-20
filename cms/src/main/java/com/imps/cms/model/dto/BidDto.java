a7514b73a48574bf7862cfd930528c0d8feebfb8
danie <danielstanpop@gmail.com>
2021-04-20 18:28:30 +0300

    Fixed bid name

--- cms/src/main/java/com/imps/cms/model/dto/BidDto.java
+++ cms/src/main/java/com/imps/cms/model/dto/BidDto.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class BitDto implements Serializable {
+public class BidDto implements Serializable {
     private Long id;
     private Long proposalId;
     private Long userId;
