f1f6fdc7f242639c4f0817616d166f7459ca0d42
Calandrinon <georgesut@yahoo.com>
2021-04-20 21:27:49 +0300

    Add the AuthenticationService
    It's still not working...

--- cms/src/main/java/com/imps/cms/angular/src/app/authentication/authentication.component.ts
+++ cms/src/main/java/com/imps/cms/angular/src/app/authentication/authentication.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "./service/authentication.service";
 
 @Component({
   selector: 'app-authentication',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authentication.component.css']
 })
 export class AuthenticationComponent implements OnInit {
+  allowed = false;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  login(email: string, password: string): void {
+    console.log("Here is where the login should start...");
+    this.authenticationService.checkCredentials(email, password);
+    setTimeout(_ => console.log("Authentication details sent."), 2000);
   }
 
 }
