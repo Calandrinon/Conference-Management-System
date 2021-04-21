ae7ba5d9c2e78314eafe9085fc5ba26309eb5803
Calandrinon <georgesut@yahoo.com>
2021-04-21 09:19:29 +0300

    Add empty profile component and redirection after login
    An error message appears if the e-mail/password are incorrect.

--- cms/src/main/java/com/imps/cms/angular/src/app/app-routing.module.ts
+++ cms/src/main/java/com/imps/cms/angular/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AuthenticationComponent } from './authentication/authentication.compone
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { PresentationsComponent } from './presentations/presentations.component';
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
   {path: "presentations", component: PresentationsComponent},
   {path: "contact", component: ContactComponent},
   {path: "auth", component: AuthenticationComponent},
+  {path: "profile", component: ProfileComponent}
 ];
 
 @NgModule({
