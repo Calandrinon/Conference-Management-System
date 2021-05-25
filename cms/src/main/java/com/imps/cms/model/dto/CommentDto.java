38ce1a72e77fcdbbe2b24f6ed39a5d4d93ed9b82
Daniel Suciu <darkleynad7@gmail.com>
2021-05-25 18:33:51 +0300

    Small commit

---
+++ cms/src/main/java/com/imps/cms/model/dto/CommentDto.java
@@ -0,0 +1,15 @@
+package com.imps.cms.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDto {
+    private Long id;
+    private Long proposalId;
+    private String userName;
+    private String content;
+}
